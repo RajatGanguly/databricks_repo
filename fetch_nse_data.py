e19e122872307e40406729a8c630dd3f69891f91
RajatGanguly <rajatganguly01@gmail.com>
2026-06-13 02:36:52 +0530

    adding pip install inside of code

--- fetch_nse_data.py
+++ fetch_nse_data.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import subprocess
+import sys
+
+subprocess.check_call(
+    [sys.executable, "-m", "pip", "install", "nsepython"]
+)
+
+from nsepython import *
+
+print(nse_get_index_quote("NIFTY 50"))
